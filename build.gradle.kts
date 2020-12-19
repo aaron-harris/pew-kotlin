f235bafd5da44c022425c0bf504d6be4206256a8
Aaron Harris <aaron.patrick.harris@gmail.com>
2020-12-19 11:49:36 -0600

    Set up Detekt for static analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
+    id("io.gitlab.arturbosch.detekt") version "1.15.0"
 }
 
 dependencies {
@@ -10,10 +11,20 @@ dependencies {
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "15"
     }
+}
+
+detekt {
+    input = files(
+        "src/main/kotlin",
+        "src/test/kotlin",
+        "build.gradle.kts"
+    )
+    config = files("src/test/resources/detekt.yml")
 }
